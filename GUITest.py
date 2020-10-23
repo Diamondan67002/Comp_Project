4fc7f2adb3cc4a6fc6847c669e4a0e5ed96955fb
dwhym <98444@students.belperschool.co.uk>
2020-10-23 12:50:02 +0100

    Initial Commit

--- GUITest.py
+++ GUITest.py
@@ -7,12 +7,20 @@ class GUI():
 
         screen = pygame.display.set_mode([500, 500])
 
+        width=screen.get_width()
+        height=screen.get_height()
+
         running = True
         while running:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     running = False
 
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    if width/2 <= mouse[0] <= width/2+140 and height/2 <= mouse[1] <= height/2+40:
+                        pygame.quit()
+
+            mouse=pygame.mouse.get_pos()
             screen.fill((255, 255, 255))
             pygame.display.flip()
 
