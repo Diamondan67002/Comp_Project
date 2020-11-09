1c2c2cbfb95889ca1ffc501ed0e7d8a7356096b2
Diamondan67002 <98444@students.belperschool.co.uk>
2020-11-09 19:30:51 +0000

    Commit 09/11/2020 GUI 3 buttons correctly placed only quit actually does something.

--- GameGUITest.py
+++ GameGUITest.py
@@ -2,6 +2,9 @@ import pygame
 import Base_Classes
 
 class Game():
+    def __init__(self):
+        self.map=Map()
+
     def build_GUI(self):
         pygame.init()
 
@@ -12,27 +15,34 @@ class Game():
         color_dark = (100, 100, 100)
         width = screen.get_width()
         height = screen.get_height()
-        smallfont = pygame.font.SysFont('Corbel', 35)
-        text = smallfont.render('Quit', True, color)
+        smallfont = pygame.font.SysFont('Corbel', 25)
+        text = [smallfont.render('Track',True,color),smallfont.render('Point',True,color),smallfont.render('Quit',True,color)]
+        positions = [[0,470],
+                     [100,470],
+                     [200,470]]### Need to move to be resizable possibly.
 
         running = True
         while running:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     running = False
-
-                if event.type == pygame.MOUSEBUTTONDOWN:  ###GforG
-                    if width / 2 <= mouse[0] <= width / 2 + 140 and height / 2 <= mouse[1] <= height / 2 + 40:
+                elif event.type == pygame.MOUSEBUTTONDOWN:  ###GforG
+                    if positions[0][0] <= mouse[0] <= positions[0][0] + 100 and positions[0][1] <= mouse[1] <= positions[0][1] + 30:
+                        ## self.map.add_Track
+                    elif positions[1][0] <= mouse[0] <= positions[1][0] + 100 and positions[1][1] <= mouse[1] <= positions[1][1] + 30:
+                        ## self.map.add_point
+                    elif positions[2][0] <= mouse[0] <= positions[2][0] + 100 and positions[2][1] <= mouse[1] <= positions[2][1] + 30:
                         running = False
 
             screen.fill((255, 255, 255))
 
             mouse = pygame.mouse.get_pos()  ###GforG the correct places.
-            if width / 2 <= mouse[0] <= width / 2 + 140 and height / 2 <= mouse[1] <= height / 2 + 40:  ###GforGsS
-                pygame.draw.rect(screen, color_light, [width / 2, height / 2, 140, 40])
-            else:
-                pygame.draw.rect(screen, color_dark, [width / 2, height / 2, 140, 40])
-            screen.blit(text, (width / 2 + 50, height / 2))
+            for i in range(3):
+                if positions[i][0] <= mouse[0] <= positions[i][0] + 100 and positions[i][1] <= mouse[1] <= positions[i][1] + 30: ### Altered from GforG
+                    pygame.draw.rect(screen, color_light, [positions[i][0],positions[i][1], 100, 30])
+                else:
+                    pygame.draw.rect(screen, color_dark, [positions[i][0],positions[i][1], 100, 30])
+                screen.blit(text[i], (positions[i][0] + 20, positions[i][1]+5))
 
             pygame.display.flip()
 
@@ -41,7 +51,6 @@ class Game():
 class Map():
     def __init__(self):
         print('Hi')
-
 
 game=Game()
 game.build_GUI()
