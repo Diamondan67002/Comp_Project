1cb35206337100003d33677bbd6e0c7dac184e75
Diamondan67002 <72200488+Diamondan67002@users.noreply.github.com>
2020-12-07 20:56:13 +0000

    Commit 07/12/2020-1 Comments Galore :-)

--- Base_Classes.py
+++ Base_Classes.py
@@ -5,8 +5,8 @@ class Track(pygame.sprite.Sprite):
     colourKey=(255,255,255)
     def __init__(self,coords,img,initConnect):### Image loading could be made bettter by preloading all the png's into python rather than loading 1 everytime an object is created or image altered
         print("Track Commenced")
-        pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.image.load(os.path.join('photos',self.images[img]))
+        pygame.sprite.Sprite.__init__(self)### Initiates the Sprite class
+        self.image = pygame.image.load(os.path.join('photos',self.images[img]))### Sets the Image for the Track
         self.image.convert_alpha()
         self.image.set_colorkey(self.colourKey)###????????
         self.rect = self.image.get_rect()
@@ -24,10 +24,10 @@ class Track(pygame.sprite.Sprite):
     def get_connection(self):### Don't really need
         return self.connections
 
-    def get_connection(self,direction):
+    def get_connection(self,direction):### Duplicated to allow for 2 different sets of arguments.
         return self.connections[direction]
 
-    def remove_connection(self):
+    def remove_connection(self):### Also duplicated to allow for difffernt arguments. Might actually be used.
         self.connections=[-1,-1]
 
     def remove_connection(self,direction):
@@ -42,13 +42,13 @@ class Track(pygame.sprite.Sprite):
     def check_vehicle(self):
         return self.vehicle
 
-    def change_orientation(self):
+    def change_orientation(self):### Using a number to represent true false given it is really neat and clean to change
         self.orientation = 1-self.orientation
 
     def change_curve(self):
         self.curve = 1-self.curve
 
-    def set_image(self,img):### Integrated into constructor
+    def set_image(self,img):### Integrated into constructor. But allows changing of images
         self.image = pygame.image.load(os.path.join('photos',self.images[img]))## Need to resolve alterations of image.
 
     def get_image(self):
@@ -59,19 +59,21 @@ class Track(pygame.sprite.Sprite):
         self.rect.x = coords[0]*32
         self.rect.y = coords[1]*32
 
-    def move_track_point(self,coords,connections):
+    def move_track_point(self,coords,connections):### Duplicated again for whether we know the new connections
         self.set_coords(coords)
+        self.remove_connection()### Added in case less new connections than old ones
         for i in range(len(connections)):
             self.add_connection(connections[i][0],connections[i][1],connections[i][2])
 
     def move_track_point(self,coords):
         self.set_coords(coords)
+        self.remove_connection()### Added to delete all of the connections. Not 100% sure if it is a point if it will call the corrrect function
 
 class Point(Track):
     images=["Point-straight.png","Point-diagonal.png"]
     def __init__(self,coords,img,initConnect):
         print("Point Commenced")
-        pygame.sprite.Sprite.__init__(self)
+        pygame.sprite.Sprite.__init__(self)### Same as track
         self.image = pygame.image.load(os.path.join('photos',self.images[img]))
         self.image.convert_alpha()
         self.image.set_colorkey(self.colourKey)  ###????????
@@ -83,18 +85,21 @@ class Point(Track):
         self.coords=coords
         self.connections = [-1,-1,-1]
         self.vehicle = ''
-        self.pointBlade = PointBlade(coords,self.colourKey)
+        self.pointBlade = PointBlade(coords,self.colourKey)### Creating the point Blade to be put over the point
         self.hand = 0
         self.add_connection(initConnect[0],initConnect[1],initConnect[2])
 
     def change_hand(self):
         self.hand = 1-self.hand
 
+    def remove_connection(self):
+        self.connections = [-1,-1,-1]
+
 class PointBlade(pygame.sprite.Sprite):### Need to add pygame.sprite.Sprite to the inheritance
-    images=["PointBlade-Straight.png","PointBlade-Curved.png"]### Might need an additional png but would alter the the changeing system unless I just rebuilt it as a 2d list.
+    images=["PointBlade-Straight.png","PointBlade-diagonal.png","PointBlade-Curved.png"]### Might need to rebuild as a 2d list.
     def __init__(self,coords,colourkey):
         self.colourkey=colourkey
-        pygame.sprite.Sprite.__init__(self)
+        pygame.sprite.Sprite.__init__(self)### Similar to before
         self.image = pygame.image.load(os.path.join('photos',self.images[0]))
         self.image.convert_alpha()
         self.image.set_colorkey(self.colourkey)  ###????????
@@ -117,22 +122,22 @@ class Siding():
         self.add_connection(initConnect[0],initConnect[1],initConnect[2])
         self.buildSiding(imgNums,self.coords,initConnect)
 
-    def add_track(self,initConnect):
+    def add_track(self,initConnect):### Appends a track object to the end of the list
         self.track.append(Track([self.coords[0],self.coords[1]+self.length],0,initConnect))
         self.length=self.length+1
 
-    def pre_add_track(self,initConnect):
+    def pre_add_track(self,initConnect):### Adds a track at the start of the list
         self.coords[1]=self.coords[1]-1
         self.track.insert(0,Track(self.coords,0,initConnect))
         self.length=self.length+1
 
-    def remove_track(self,sidingPos):
-        self.track.remove(sidingPos)
+    def remove_track(self,sidingPos):### Removes a track but need to write the algoritmn that goes and reconfigure both the connections and all the coordinates.
+        self.track.remove(sidingPos)### There is quite a few possibilities there though.
 
-    def buildSiding(self,imgNums,coords,initConnect):
+    def buildSiding(self,imgNums,coords,initConnect):### Ran upon initiation
         for i in range(self.length):
             if i>0:
-                initConnect[2]=initConnect[2]+1
+                initConnect[2]=initConnect[2]+1### Need to get it to go back thoug and add all the connections the other way as well.
             self.track.append(Track(coords,imgNums[i],initConnect))### Need to set up all the alignments of x and y coords coming down from the initial map function.
             coords[1]=coords[1]+1
 
@@ -145,7 +150,7 @@ class Siding():
     def get_connection(self,direction):
         return self.connections[direction]
 
-    def get_track_connection(self,sidingPos):
+    def get_track_connection(self,sidingPos):### Again duplication for different arguments
         return self.track[sidingPos].get_connections()
 
     def get_track_connection(self,direction,sidingPos):
@@ -163,7 +168,10 @@ class Siding():
     def get_track(self,sidingPos):
         return self.track[sidingPos]
 
-class DeadEndSiding(Siding):
+    def set_track_coords(self,coords,sidingPos):
+        self.track[sidingPos].set_coords(coords)
+
+class DeadEndSiding(Siding):### Don't think it will every actually be used.
     def __init__(self):
         super().__init__()###???????????????????
         self.connections=[-1]
@@ -179,23 +187,23 @@ class Line():### A Line probably going to have a fixed y value but could be alte
 
     def buildLine(self,setup,coords,connections):### Actually builds and configures the line. Passing variable into Point() and Siding()
         for i in range(len(setup)):
-            if setup[i][0]==-1:
+            if setup[i][0]==-1:### If it has a value of -1 then it should be a Point()
                 self.line.append(Point(coords,setup[i][1],[0,connections[0],connections[1]]))#### Need to continue passing though the GUI coords.
                 coords[0]=coords[0]+1
                 connections[1]=connections[1]+1
-            elif setup[i][0]>=1:
+            elif setup[i][0]>=1:### Is a Siding of length setup[i][0]
                 self.line.append(Siding(setup[i][0],coords,setup[i][1],[0,connections[0],connections[1]]))### Issues. Coords and connections are effectively duplicates that should be in tegrated together.
                 coords[0]=coords[0]+setup[i][0]
                 connections[1]=connections[1]+setup[i][0]
 
     def get_component_no(self,x_coord):### Can we chack what type of object an item in a list is??
-        x = 0
+        x = 0 ### This converts a normal x_coord into a linePos and sidingPos where if it is a Point sidingPos = False
         linePos = 0
         running = True
-        while running:
-            if len(self.line[x].get_connections()) == 2:
+        while running:### Goes and iterates though until the x reaches or is greater than x_coord
+            if len(self.line[x].get_connections()) == 3:### Had these 2 the wrong way around
                 x +=1
-            elif len(self.line[x].get_connections()) == 3:
+            elif len(self.line[x].get_connections()) == 2:
                 x = x + self.line[x].check_length()
 
             linePos += 1
@@ -217,26 +225,26 @@ class Line():### A Line probably going to have a fixed y value but could be alte
             return self.line[linePos].check_vehicle()
         return self.line[linePos].get_vehicle(sidingPos)
 
-    def remove_vehicle(self,x_coord):
+    def remove_vehicle(self,x_coord):### Removes a vehicle at a certain x_coord
         linePos, sidingPos = self.get_component_no(x_coord)
         if sidingPos == False:
             self.line[linePos].remove_vehicle()
         else:
             self.line[linePos].remove_vehicle(sidingPos)
 
-    def move_vehicle(self,x_coord,direction):
+    def move_vehicle(self,x_coord,direction):### Moves a vehicle 1 space
         wagon = self.get_vehicle(x_coord)
         self.remove_vehicle(x_coord)
         new_location = self.get_connection(x_coord,direction)
         return wagon, new_location
 
-    def get_connection(self,x_coord,direction):
+    def get_connection(self,x_coord,direction):### Gets a connection for a Track()/Point()
         linePos, sidingPos = self.get_component_no(x_coord)#### Should I move these calls into the previous method as move_vehicle() is calling it a lot of times.
         if sidingPos == False:
             return self.line[linePos].get_connection(direction)
         return self.line[linePos].get_track_connection(direction,sidingPos)
 
-    def get_track_point(self,x_coord):
+    def get_track_point(self,x_coord):### Returns a Track/Point object to be moved
         linePos, sidingPos = self.get_component_no(x_coord)
         if sidingPos == False:
             return self.line[linePos]
@@ -248,7 +256,9 @@ class Line():### A Line probably going to have a fixed y value but could be alte
             self.line.remove(linePos)
         self.line[linePos].remove_track(sidingPos)
 
-    def set_track_point_coords(self,coords):
+    def set_track_point_coords(self,coords):### Sets a Track()/Point()'s internal coords
         linePos, sidingPos = self.get_component_no(coords[1])
         if sidingPos == False:
-            self.line[linePos]
+            self.line[linePos].set_coords(coords)
+        else:
+            self.line[linePos].set_track_coords(coords,sidingPos)
--- GameGUITest.py
+++ GameGUITest.py
@@ -4,7 +4,7 @@ import Base_Classes
 class Game():
     sprites = pygame.sprite.Group()
 
-    positions = [[0, 470, 100, 30],
+    positions = [[0, 470, 100, 30],### Positions of all the buttons/rectangles
                  [100, 470, 100, 30],
                  [200, 470, 100, 30],
                  [300, 470, 100, 30],
@@ -24,19 +24,19 @@ class Game():
         width = screen.get_width()
         height = screen.get_height()
         smallfont = pygame.font.SysFont('Corbel', 25)
-        text = [smallfont.render('Track',True,color),smallfont.render('Point',True,color),smallfont.render('Quit',True,color),smallfont.render('Inglenook',True,color),False]
+        text = [smallfont.render('Track',True,color),smallfont.render('Point',True,color),smallfont.render('Quit',True,color),smallfont.render('Inglenook',True,color),False]### Text for all the buttons
         colors = [0,0,0,0,1]### Probably going to need to make self for all of these lists and move them out of the functions
-        color_pairs = [[color_light,color_dark],
+        color_pairs = [[color_light,color_dark],### paired colors for the variations if it is being hovered over.
                        [(220,200,0),(255,255,0)]]
 
         running = True
         while running:
             mouse = pygame.mouse.get_pos()  ###GforG the correct places.
 
-            for event in pygame.event.get():
+            for event in pygame.event.get():### Checks for all events that have happened
                 if event.type == pygame.QUIT:
                     running = False
-                elif event.type == pygame.MOUSEBUTTONDOWN:  ###GforG
+                elif event.type == pygame.MOUSEBUTTONDOWN:  ###GforG ### CHecks for presses of the GUI buttons
                     if self.positions[0][0] <= mouse[0] <= self.positions[0][0] + self.positions[0][2] and self.positions[0][1] <= mouse[1] <= self.positions[0][1] + self.positions[0][3]:
                         self.sprites.add(self.map.add_track())
                     elif self.positions[1][0] <= mouse[0] <= self.positions[1][0] + self.positions[1][2] and self.positions[1][1] <= mouse[1] <= self.positions[1][1] + self.positions[1][3]:
@@ -45,7 +45,7 @@ class Game():
                         running = False
                     elif self.positions[3][0] <= mouse[0] <= self.positions[3][0] + self.positions[3][2] and self.positions[3][1] <= mouse[1] <= self.positions[3][1] + self.positions[3][3]:
                         self.add_list_sprites(self.map.build_inglenook())### This was my idea bit.
-                elif event.type == pygame.KEYDOWN:
+                elif event.type == pygame.KEYDOWN:### Checks though all the key operations
                     if event.key == pygame.K_KP8:
                         self.move_mover_y(-1)
                     elif event.key == pygame.K_KP2:
@@ -66,7 +66,7 @@ class Game():
             screen.fill((255, 255, 255))
 
 
-            for i in range(len(self.positions)):
+            for i in range(len(self.positions)):### Changes the rectangle colors if the mouse is hovered over them.
                 if self.positions[i][0] <= mouse[0] <= self.positions[i][0] + self.positions[i][2] and self.positions[i][1] <= mouse[1] <= self.positions[i][1] + self.positions[i][3]: ### Altered from GforG
                     pygame.draw.rect(screen, color_pairs[colors[i]][0], [self.positions[i][0],self.positions[i][1],self.positions[i][2],self.positions[i][3]])
                 else:
@@ -84,24 +84,24 @@ class Game():
     def add_sprite(self,sprite):### Probably don't need.
         self.sprites.add(sprite)
 
-    def add_list_sprites(self,sprites):
+    def add_list_sprites(self,sprites):### Adds all the objects in a list to a group
         for i in range(len(sprites)):
             self.add_sprite(sprites[i])
 
-    def move_selector_x(self,direction):### Need to make sure it doesn't go off the edges of the screen.
-        if 0 <= self.positions[4][0] + direction <= 500:
+    def move_selector_x(self,direction):### Need to make sure it doesn't go off the edges of the screen. Fixed
+        if 0 <= self.positions[4][0] + direction <= 500:### Moves the selector in the x direction
             self.positions[4][0]=self.positions[4][0] + direction
 
-    def move_selector_y(self,direction):
+    def move_selector_y(self,direction):### Moves the selector in the y direction
         if 0 <= self.positions[4][1] + direction <= 500:
             self.positions[4][1] = self.positions[4][1] + direction
 
-    def move_mover_x(self,direction):
+    def move_mover_x(self,direction):### Moves the mover and slector and therefore a component where the selector is in the x direction
         coords = [self.positions[4][0]/32,self.positions[4][1]/32]
         self.map.move_component_x(coords)
         self.move_selector_x(direction*32)### Converting position movement into Pixels.
 
-    def move_mover_y(self,direction):
+    def move_mover_y(self,direction):### Moves the mover and slector and therefore a component where the selector is in the y direction
         coords = [self.positions[4][0] / 32, self.positions[4][1] / 32]
         self.map.move_component_y(coords)
         self.move_selector_y(direction*32)
@@ -131,7 +131,7 @@ class Map():
             self.map.append(Base_Classes.Line(setup[i],[0,startCoords[1]+i],connections[i]))##need to configure the y coords and the orientations
 
     def move_component_x(self,coords):## Would have a issue if we  had the start cooords of 0,0
-        print(Hi)
+        print("Hi")
 
     def move_component_y(self,coords):
         print("Hi")
